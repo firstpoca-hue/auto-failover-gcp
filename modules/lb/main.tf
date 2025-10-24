604aa5de206067aac6453bc0633317bb9792c862
Sashangy Kumar <sashangy.kumar@atos.net>
2025-10-24 19:30:43 +0530

    Fix neg

--- modules/lb/main.tf
+++ modules/lb/main.tf
@@ -1,46 +1,48 @@
+# 🟩 modules/lb/main.tf
+
 resource "google_compute_global_address" "lb_ip" {
   name = "${var.name}-ip"
 }
 
+# 🟩 Ensure valid PEMs exist (generated via OpenSSL)
 resource "google_compute_ssl_certificate" "self_signed" {
-  name = "${var.name}-self-signed"
-  private_key = file("${path.module}/certs/private_key.pem")
-  certificate = file("${path.module}/certs/certificate.pem")
+  name        = "${var.name}-self-signed"
+  private_key = file("${path.module}/certs/private.key")
+  certificate = file("${path.module}/certs/certificate.csr")
 }
 
+# 🟩 Use HTTPS protocol since LB proxy is HTTPS
 resource "google_compute_backend_service" "backend" {
   name          = var.lb_name
-  protocol      = "HTTP"
-  port_name     = "http"
+  protocol      = "HTTPS"                       # 🟩 changed from HTTP → HTTPS
+  port_name     = "https"                       # 🟩 changed
   timeout_sec   = 30
   health_checks = [google_compute_health_check.default.self_link]
+
   backend {
-    group = var.neg  # your NEG/MIG
+    # 🟩 Expect zonal NEG self-link from GKE (global LBs need ZONAL NEGs)
+    group = var.neg
   }
 }
 
 resource "google_compute_url_map" "urlmap" {
-  name = "${var.name}-urlmap"
+  name            = "${var.name}-urlmap"
   default_service = google_compute_backend_service.backend.id
 }
 
 resource "google_compute_target_https_proxy" "proxy" {
-  name = "${var.name}-proxy"
-  url_map = google_compute_url_map.urlmap.id
+  name             = "${var.name}-proxy"
+  url_map          = google_compute_url_map.urlmap.id
   ssl_certificates = [google_compute_ssl_certificate.self_signed.id]
 }
 
 resource "google_compute_global_forwarding_rule" "fwd" {
-  name = "${var.name}-fwd"
-  load_balancing_scheme = "EXTERNAL_MANAGED"
-  target = google_compute_target_https_proxy.proxy.id
-  port_range = "443"
+  name                  = "${var.name}-fwd"
+  load_balancing_scheme = "EXTERNAL"
+  target                = google_compute_target_https_proxy.proxy.id
+  port_range            = "443"
+  ip_address            = google_compute_global_address.lb_ip.address
 }
-
-output "forwarding_ip" { 
-value = google_compute_global_address.lb_ip.address 
-}
-
 
 resource "google_compute_health_check" "default" {
   name               = "app-hc"
@@ -48,7 +50,11 @@ resource "google_compute_health_check" "default" {
   timeout_sec        = 5
 
   http_health_check {
-    port = var.health_check_port
+    port         = var.health_check_port
     request_path = var.health_check_path
   }
+}
+
+output "forwarding_ip" {
+  value = google_compute_global_address.lb_ip.address
 }
