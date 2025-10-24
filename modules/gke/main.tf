70db91b82ec26f9ba168c7813291ab47cd52cc70
Sashangy Kumar <sashangy.kumar@atos.net>
2025-10-24 12:44:06 +0530

    Added workflows

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -1,28 +1,37 @@
+# 🔹 Create cluster only if enabled == true
 resource "google_container_cluster" "this" {
-  count = var.enabled ? 1 : 0
-  name  = "${var.name}-cluster"
-  location = var.region
-  network = var.network_id
-  subnetwork = var.subnetwork_id
-  remove_default_node_pool = true
-  initial_node_count = 1
-  release_channel { 
-  channel = "REGULAR" 
+  count                     = var.enabled ? 1 : 0          # CHANGED
+  name                      = "${var.name}-cluster"
+  location                  = var.region
+  network                   = var.network_id
+  subnetwork                = var.subnetwork_id
+  remove_default_node_pool  = true
+  initial_node_count        = 1
+
+  release_channel {
+    channel = "REGULAR"
   }
 }
 
+# 🔹 Node pool depends on the cluster
 resource "google_container_node_pool" "nodes" {
-  count = var.enabled ? 1 : 0
-  name = "${var.name}-pool"
-  cluster = google_container_cluster.this[0].name
+  count    = var.enabled ? 1 : 0                           # CHANGED
+  name     = "${var.name}-pool"
+  cluster  = google_container_cluster.this[0].name
   location = var.region
   node_count = 2
+
   node_config {
-   machine_type = "e2-medium" 
+    machine_type = "e2-medium"
   }
 }
 
-# NOTE: Kubernetes NEG is created by a k8s Service with NEG annotation; this is a placeholder output
+# Output only if cluster is created
+output "cluster_name" {
+  value       = var.enabled ? google_container_cluster.this[0].name : null  # CHANGED
+  description = "GKE cluster name"
+}
+
 output "neg_self_link" {
-  value = "/projects/${var.name}/regions/${var.region}/networkEndpointGroups/app-service-neg"
+  value = var.enabled ? "/projects/${var.name}/regions/${var.region}/networkEndpointGroups/app-service-neg" : null
 }
