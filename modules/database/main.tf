a62a97a498580311a54f2fa01c4193f72e4e10fa
Sashangy Kumar <sashangy.kumar@atos.net>
2025-10-27 21:30:37 +0530

    fix for the Cloud SQL

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -37,6 +37,30 @@ resource "google_sql_database_instance" "primary" {
       binary_log_enabled = true
     }
   }
+
+  replica_configuration {
+    failover_target = false
+  }
+}
+
+
+
+resource "google_sql_database_instance" "replica" {
+  count                = var.enable_replica ? 1 : 0
+  name                 = "app-db-replica-${random_id.db_name_suffix.hex}"
+  region               = var.secondary_region
+  database_version     = var.database_version
+  deletion_protection  = false
+  master_instance_name = google_sql_database_instance.primary.name
+
+  settings {
+    tier = var.db_tier
+    
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = var.network
+    }
+  }
 }
 
 output "primary_connection_name" {
@@ -46,3 +70,12 @@ output "primary_connection_name" {
 output "primary_private_ip" {
   value = google_sql_database_instance.primary.private_ip_address
 }
+
+output "replica_connection_name" {
+  value = var.enable_replica ? google_sql_database_instance.replica[0].connection_name : null
+}
+
+output "replica_private_ip" {
+  value = var.enable_replica ? google_sql_database_instance.replica[0].private_ip_address : null
+}
+
