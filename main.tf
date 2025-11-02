3d32d87aaf2f307095c9736f55bc420724d7e179
Sashangy Kumar <sashangy.kumar@atos.net>
2025-11-02 11:57:29 +0530

    added new code

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-resource "null_resource" "sql_dependency" {
-  depends_on = [module.database]
-}
+# resource "null_resource" "sql_dependency" {
+#   depends_on = [module.database]
+# }
 
 module "network" {
   source = "./modules/network"
@@ -9,7 +9,7 @@ module "network" {
   secondary_region = var.secondary_region
   psa_range_name = var.psa_range_name
   psa_prefix_length = var.psa_prefix_length
-  depends_on = [null_resource.sql_dependency]
+  # depends_on = [null_resource.sql_dependency]
 }
 
 # --- PRIMARY GKE CLUSTER (always created)
