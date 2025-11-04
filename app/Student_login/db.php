232eeaf45ee21e194103b9e5fb98bada66c6b77c
Amit KUMAR-SHAW <amit.kumar-shaw.ext@veolia.com>
2025-11-04 16:59:48 +0530

    app

--- app/Student_login/db.php
+++ app/Student_login/db.php
@@ -7,12 +7,8 @@ $password = 'StrongPassword123!';
 try {
     $options = [
         PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-        PDO::MYSQL_ATTR_SSL_CA => null,
-        PDO::MYSQL_ATTR_SSL_CERT => null,
-        PDO::MYSQL_ATTR_SSL_KEY => null,
-        PDO::MYSQL_ATTR_SSL_MODE => PDO::MYSQL_SSL_MODE_DISABLED,
     ];
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;sslmode=disable", $username, $password, $options);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $options);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
